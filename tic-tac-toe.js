4e169c5e4542ac4fd1cddcbdc160eb4e4e7d4c3f
Ruth-Ann Blair <blair.ruthie1@gmail.com>
2022-10-13 22:14:03 -0500

    Exercise 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", (event)=> {
+    let squares  = document.querySelectorAll("#board > div")
+    squares.forEach((sq)=>{
+        sq.classList.add("square");
+    });
+});
